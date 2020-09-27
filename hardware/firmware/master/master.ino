5a847344550575de82b4d7c0ed8de2d5e375590b
Keshara Weerasinghe <keshara2032@gmail.com>
2020-09-27 17:44:08 +0530

    Basic Firmware with Pump Support

--- hardware/firmware/master/master.ino
+++ hardware/firmware/master/master.ino
@@ -1,10 +1,13 @@
 #define dirPin 6
 #define pulPin 7
-#define hallSensor A7
+#define hallSensor 5
+#define relaySig 9
 
-int hallSensorReading = 1;
+
+int hallSensorReading =0;
 int dir = 0;
-int speed = 250;
+int speed = 200;
+int turn=LOW;
 
 void setup() {
 
@@ -12,41 +15,48 @@ void setup() {
   // Declare pins as output:
   pinMode(pulPin, OUTPUT);
   pinMode(dirPin, OUTPUT);
+  pinMode(relaySig, OUTPUT);
   pinMode(hallSensor, INPUT);
   digitalWrite(dirPin, dir);
 
-  // Set the spinning direction CW/CCW:
+  turn=HIGH; // Initially turn irrespective of location
+
 }
+
 void loop() {
-  // These four lines result in 1 step:
-  hallSensorReading = analogRead(hallSensor);
 
-  Serial.println(hallSensorReading);
-
- if(hallSensorReading < 500){
+  
+ if(turn){
   runMotor(dir, speed);
  }
+
+// Disk has reached correct position. Filling starts
+ digitalWrite(relaySig,HIGH); // Pump ON
+ delay(1000); // Run pump for 1000ms
+ digitalWrite(relaySig,LOW); // Pump OFF
+ delay(1000);
+ 
+ turn = HIGH; // Start Turning
+ hallSensorReading = 0;
+
 }
 
 void runMotor(int dir, int speed){
   
     for (int i=0; i<6400; i++)    //Forward 5000 steps
   {
-    hallSensorReading = analogRead(hallSensor);
-    if(hallSensorReading > 500) break;
+    // Check for position
+    hallSensorReading = digitalRead(hallSensor);
+    if(hallSensorReading == 1 && !turn && i>3200) break; // Stop if correct position
+    
     digitalWrite(dirPin,LOW);
     digitalWrite(pulPin,HIGH);
     delayMicroseconds(speed);
     digitalWrite(pulPin,LOW);
     delayMicroseconds(speed);
+    
+    if(hallSensorReading == 0) turn = LOW;
   }
-//  for (int i=0; i<6400; i++)   //Backward 5000 steps
-//  {
-//  digitalWrite(dirPin,HIGH);
-//    digitalWrite(pulPin,HIGH);
-//    delayMicroseconds(speed);
-//    digitalWrite(pulPin,LOW);
-//    delayMicroseconds(speed);
-//  }
+
 }
 
